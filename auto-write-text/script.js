b5ff0d92b2db03df9ab875de7073cff58fd21ee9
wping <wen240979@gmail.com>
2021-07-22 21:30:15 +0800

    added project.

---
+++ auto-write-text/script.js
@@ -0,0 +1,13 @@
+const text = "I love frontend development!! <3";
+
+let index = 0;
+
+function writeText(){
+    document.body.innerText = text.slice(0,index);
+    index ++;
+    if(index > text.length){
+        index = 0;
+    }
+}
+
+setInterval(writeText,100);
